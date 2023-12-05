e2a2df71e5fc6dcb5a276793be4baf4c575183b1
Toshin Prince <toshinprince@gmail.com>
2023-12-05 22:52:30 +0530

    routes

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
+//conect DB
+
+//routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+
+// app.use("/api/v1");
+app.get("/", (req, res) => {
+  res.send("jobs api");
 });
 
 app.use(notFoundMiddleware);
