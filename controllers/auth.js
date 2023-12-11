0591d9e258a7b56f9c7af01f0685076850487408
Toshin Prince <toshinprince@gmail.com>
2023-12-11 20:18:40 +0530

    mongoose middleware for hashing and jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,16 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const user = await User.create({ ...req.body });
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
 
-  const tempUser = { name, email, password: hashedPassword };
-
-  //We are using Mongoose validators i.e we do not need our own
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError("Please Provide Name, email and password");
-  // }
-
-  const user = await User.create({ ...tempUser });
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
