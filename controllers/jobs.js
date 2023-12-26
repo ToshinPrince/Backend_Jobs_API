31d347a24ee78cb45f7a64e7df6b78ff4d3c9346
Toshin Prince <toshinprince@gmail.com>
2023-12-26 14:27:02 +0530

    updateJob

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -27,7 +27,26 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send("Update Job");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or postion fields can not be Empty");
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const deleteJob = async (req, res) => {
