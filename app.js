2ac39b14ad9612e72fe2aaea914ab98b18f7347c
Toshin Prince <toshinprince@gmail.com>
2024-01-02 16:20:20 +0530

    Security

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//Extra Security Package
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const authenticateUser = require("./middleware/authentication");
@@ -15,8 +22,11 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(rateLimiter);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss);
 
 // routes
 app.use("/api/v1/auth", authRouter);
