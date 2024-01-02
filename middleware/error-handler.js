c7a2910b43a6d8986ba29e7f654c66e0ec97cf3c
Toshin Prince <toshinprince@gmail.com>
2024-01-02 16:04:27 +0530

    CastError Update

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -25,6 +25,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field, please choose another Value`;
     customError.statusCode = 400;
   }
+
+  if (err.name === "CastError") {
+    customError.msg = `No Item found with id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
